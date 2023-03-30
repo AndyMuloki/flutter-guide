8c6ddb9bef8d88032c391586ffebaa1f3deb6d6e
Andrew <andrew.muloki@strathmore.edu>
2023-03-31 00:55:11 +0300

    add result feedback

--- lib/result.dart
+++ lib/result.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({Key key}) : super(key: key);
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'Awesome, you\'re pretty cool!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty Likeable!';
+    } else if (resultScore <= 16) {
+      resultText = 'Bah!';
+    } else {
+      resultText = "Bollocks!!";
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
